f115808b495ea4096fb2e3f8daffd6d8a8701b9f
sarra.bousnina@esprit.tn <sarra.bousnina@esprit.tn>
2025-10-20 17:07:46 +0100

    sidaber fix

--- frontend/src/components/RootLayout.tsx
+++ frontend/src/components/RootLayout.tsx
@@ -1,13 +1,18 @@
-import { Outlet } from "react-router-dom";
+// src/layouts/RootLayout.tsx (or wherever it is)
+import { Outlet, useLocation } from "react-router-dom";
 import Sidebar from "./sidebar/Sidebar";
 import "./sidebar/Sidebar.css";
 
 export default function RootLayout() {
+  const location = useLocation();
+
+  // Hide sidebar on auth pages
+  const hideSidebar = ["/login", "/signup"].includes(location.pathname);
+
   return (
     <>
-      <Sidebar />
-      <main className="sb-main">
-        {/* This wrapper constrains page width nicely */}
+      {!hideSidebar && <Sidebar />}
+      <main className={`sb-main ${hideSidebar ? "full-width" : ""}`}>
         <div className="page">
           <Outlet />
         </div>
@@ -15,4 +20,3 @@ export default function RootLayout() {
     </>
   );
 }
-
